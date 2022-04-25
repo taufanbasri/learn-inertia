842ee6bede655dd32fc12a24f0f4976544ae387b
taufanbasri <taufanprasetyobasri@gmail.com>
2022-04-26 02:39:57 +0700

    Basic Pages

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Hello, {{ name }}</h1>
+
+  <p>In this series, we will use the following frameworks:</p>
+  <ul>
+      <li v-for="(framework, index) of frameworks" v-text="framework" :key="index"></li>
+  </ul>
+</template>
+
+<script>
+export default {
+    props: {
+        name: String,
+        frameworks: Array
+    }
+}
+</script>
+
+<style>
+
+</style>
