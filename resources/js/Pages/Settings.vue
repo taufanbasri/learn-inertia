74376ac4fe9d788d4f2a54a399b9acd44c6a21cc
taufanbasri <taufanprasetyobasri@gmail.com>
2022-05-01 09:06:53 +0700

    Default Layouts

--- resources/js/Pages/Settings.vue
+++ resources/js/Pages/Settings.vue
@@ -2,14 +2,3 @@
     <h1 class="text-3xl">Settings</h1>
 </template>
 
-<script>
-import Layout from "../Shared/Layout.vue";
-
-export default {
-    layout: Layout
-}
-</script>
-
-<style>
-
-</style>
--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -14,18 +14,10 @@
 </template>
 
 <script>
-import { Link } from "@inertiajs/inertia-vue3";
-import Layout from '../Shared/Layout.vue';
-
 export default {
-    layout: Layout,
 
     props: {
         time: String
     }
 }
 </script>
-
-<style>
-
-</style>
