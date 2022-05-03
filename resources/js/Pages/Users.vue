78efe2fe78400952228d12265def4436164ca67e
taufanbasri <taufanprasetyobasri@gmail.com>
2022-05-03 22:48:34 +0700

    Filtering, State & Query Strings

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,7 +1,11 @@
 <template>
     <Head title="Users"/>
 
-    <h1 class="text-3xl">Users</h1>
+    <div class="flex justify-between mb-6">
+        <h1 class="text-3xl">Users</h1>
+
+        <input v-model="search" type="text" placeholder="Search..." class="px-2 border rounded-lg">
+    </div>
 
     <div class="flex flex-col">
         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
@@ -35,7 +39,22 @@
 </template>
 
 <script setup>
+import { Inertia } from "@inertiajs/inertia"
+import { ref } from "@vue/reactivity"
+import { watch } from "@vue/runtime-core"
 import Pagination from "../Shared/Pagination.vue"
 
-defineProps({ users: Object })
+let props = defineProps({
+    users: Object,
+    filters: Object
+})
+
+let search = ref(props.filters.search)
+
+watch(search, value => {
+    Inertia.get('/users', { search: value }, {
+        preserveState: true,
+        replace: true
+    })
+})
 </script>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\User;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -21,10 +22,18 @@ Route::get('/', function () {
 
 Route::get('/users', function () {
     return Inertia::render('Users', [
-        'users' => User::paginate(10)->through(fn($user) => [
-            'id' => $user->id,
-            'name' => $user->name // Only pass this field to client side
-        ])
+        'users' => User::query()
+                ->when(Request::input('search'), function ($query, $search) {
+                    $query->where('name', 'like', "%{$search}%");
+                })
+                ->paginate(10)
+                ->withQueryString()
+                ->through(fn($user) => [
+                    'id' => $user->id,
+                    'name' => $user->name // Only pass this field to client side
+                ]),
+
+        'filters' => Request::only(['search'])
     ]);
 });
 
